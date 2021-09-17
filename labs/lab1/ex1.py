170b5e287b868b5fde3c9adcf13e1baefd17dfb7
James R T <jamestiotio@gmail.com>
2021-09-17 10:24:53 +0800

    Add final lab 1 solutions

--- labs/lab1/ex1.py
+++ labs/lab1/ex1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # SUTD 50.042 FCS Lab 1
-# Simple file read in/out
+# Simple shift cipher with auxiliary file read in/out capabilities
 
 
 # Import libraries
@@ -8,14 +8,20 @@ import sys
 import argparse
 import string
 
-
+# Define a bunch of input argument validators
 def key_type(k):
-    k = int(k)
-    if not 1 <= k <= len(string.printable) - 1:
+    try:
+        k = int(k)
+        # Use %= len(string.printable) to allow and accommodate for key overflows outside of specified range
+        if not 1 <= k <= len(string.printable) - 1:
+            raise argparse.ArgumentTypeError(
+                "Please input a valid key between 1 and len(string.printable) - 1, inclusive."
+            )
+        return k
+    except ValueError:
         raise argparse.ArgumentTypeError(
-            "Please input a valid key between 1 and len(string.printable) - 1, inclusive."
+            "Please input an integer between 1 and len(string.printable) - 1, inclusive, for the key."
         )
-    return k
 
 
 def mode_type(m):
@@ -27,13 +33,13 @@ def mode_type(m):
     return m
 
 
-def doStuff(filein, fileout, key, mode):
+def do_stuff(filein, fileout, key, mode):
+    shifted_printables = string.printable[key:] + string.printable[:key]
     output_text = ""
     # PROTIP: Pythonic way to open file handle in read mode
     with open(filein, mode="r", encoding="utf-8", newline="\n") as fin:
         text = fin.read()
-        # do stuff
-        shifted_printables = string.printable[key:] + string.printable[:key]
+        # Do stuff
         if mode == "e":
             for c in text:
                 idx = string.printable.index(c)
@@ -42,29 +48,29 @@ def doStuff(filein, fileout, key, mode):
             for c in text:
                 idx = shifted_printables.index(c)
                 output_text += string.printable[idx]
-        # file stream will be closed automatically when interpreter reaches end of the block
+        # File stream will be closed automatically when interpreter reaches end of the block
 
-    # write to fileout (open in another separate context to prevent simultaneous file opening/modification and race conditions)
+    # Write to fileout (open in another separate context to prevent simultaneous file opening/modification and race conditions)
     with open(fileout, mode="w", encoding="utf-8", newline="\n") as fout:
         fout.write(output_text)
 
 
-# our main function
+# Our main function
 if __name__ == "__main__":
-    # set up the argument parser
+    # Set up the argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", dest="filein", help="input file")
+    parser.add_argument("-i", dest="filein", help="input file", default="sherlock.txt")
     parser.add_argument("-o", dest="fileout", help="output file", default="output.txt")
-    parser.add_argument("-k", dest="key", help="key", type=key_type)
-    parser.add_argument("-m", dest="mode", help="mode", type=mode_type)
+    parser.add_argument("-k", dest="key", help="key", type=key_type, default=1)
+    parser.add_argument("-m", dest="mode", help="mode", type=mode_type, default="e")
 
-    # parse our arguments
+    # Parse our arguments
     args = parser.parse_args()
     filein = args.filein
     fileout = args.fileout
     key = args.key
     mode = args.mode
 
-    doStuff(filein, fileout, key, mode)
+    do_stuff(filein, fileout, key, mode)
 
-    # all done
+    # All done
--- labs/lab1/ex2.py
+++ labs/lab1/ex2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # SUTD 50.042 FCS Lab 1
-# Simple file read in/out
+# Simple shift cipher with auxiliary file read in/out capabilities
 
 
 # Import libraries
@@ -8,14 +8,20 @@ import sys
 import argparse
 import string
 
-
+# Define a bunch of input argument validators
 def key_type(k):
-    k = int(k)
-    if not 1 <= k <= len(string.printable) - 1:
+    try:
+        k = int(k)
+        # Use %= 256 to allow and accommodate for key overflows outside of specified range
+        if not 0 <= k <= 255:
+            raise argparse.ArgumentTypeError(
+                "Please input a valid key between 0 and 255, inclusive."
+            )
+        return k
+    except ValueError:
         raise argparse.ArgumentTypeError(
-            "Please input a valid key between 1 and len(string.printable) - 1, inclusive."
+            "Please input an integer between 0 and 255, inclusive, for the key."
         )
-    return k
 
 
 def mode_type(m):
@@ -27,31 +33,43 @@ def mode_type(m):
     return m
 
 
-def doStuff(filein, fileout, key, mode):
+def do_stuff(filein, fileout, key, mode):
+    output_bin = []
     # PROTIP: Pythonic way to open file handle in binary read mode
-    with open(filein, mode="rb") as fin:
-        text = fin.read()
-        # do stuff
+    with open(filein, mode="rb") as fin_b:
+        text = fin_b.read()
+        # Do stuff
+        if mode == "e":
+            for p in text:
+                c = (p + key) % 256
+                output_bin.append(c)
+        elif mode == "d":
+            for c in text:
+                p = (c - key) % 256
+                output_bin.append(p)
+        # File stream will be closed automatically when interpreter reaches end of the block
 
-        # file stream will be closed automatically when interpreter reaches end of the block
+    # Write to fileout (open in another separate context to prevent simultaneous file opening/modification and race conditions)
+    with open(fileout, mode="wb") as fout_b:
+        fout_b.write(bytearray(output_bin))
 
 
-# our main function
+# Our main function
 if __name__ == "__main__":
-    # set up the argument parser
+    # Set up the argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", dest="filein", help="input file")
+    parser.add_argument("-i", dest="filein", help="input file", default="sherlock.txt")
     parser.add_argument("-o", dest="fileout", help="output file", default="out")
-    parser.add_argument("-k", dest="key", help="key", type=key_type)
-    parser.add_argument("-m", dest="mode", help="mode", type=mode_type)
+    parser.add_argument("-k", dest="key", help="key", type=key_type, default=0)
+    parser.add_argument("-m", dest="mode", help="mode", type=mode_type, default="e")
 
-    # parse our arguments
+    # Parse our arguments
     args = parser.parse_args()
     filein = args.filein
     fileout = args.fileout
     key = args.key
     mode = args.mode
 
-    doStuff(filein, fileout, key, mode)
+    do_stuff(filein, fileout, key, mode)
 
-    # all done
+    # All done
