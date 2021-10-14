b8876c8e94aff7ce35249df52822bf4b1acb4821
James R T <jamestiotio@gmail.com>
2021-10-14 12:30:30 +0800

    Add some minor lab changes

---
+++ labs/lab4/extract.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 extract.py -i letter.e -o decrypted_letter_result.pbm -hh header.pbm
--- labs/lab5/gf2n.py
+++ labs/lab5/gf2n.py
@@ -4,18 +4,32 @@
 # James Raphael Tiovalen / 1004555
 
 import copy
+from itertools import zip_longest
 
 
 # Possible improvement would be to use a SageMath and/or NumPy-based implementation
 class Polynomial2:
     def __init__(self, coeffs):
-        pass
+        self._coeffs = copy.deepcopy(coeffs)
+
+    @property
+    def degree(self):
+        return len(self._coeffs) - 1
 
     def add(self, p2):
-        pass
+        new_coeffs = []
+
+        for x, y in zip_longest(self._coeffs, p2._coeffs, fillvalue=0):
+            result.append(x ^ y)
+
+        # Remove any trailing zeroes for higher order bits
+        while len(new_coeffs) > 1 and new_coeffs[-1] == 0:
+            new_coeffs.pop(-1)
+
+        return Polynomial2(new_coeffs)
 
     def sub(self, p2):
-        pass
+        return self.add(p2)
 
     # Possible improvement:
     # - https://hal.archives-ouvertes.fr/hal-02070816/document
@@ -29,6 +43,9 @@ class Polynomial2:
 
     def __str__(self):
         pass
+
+    def __eq__(self, p2):
+        return str(self) == str(p2)
 
     def getInt(p):
         pass
@@ -77,82 +94,82 @@ class GF2N:
         pass
 
 
-print "\nTest 1"
-print "======"
-print "p1=x^5+x^2+x"
-print "p2=x^3+x^2+1"
+print("\nTest 1")
+print("======")
+print("p1=x^5+x^2+x")
+print("p2=x^3+x^2+1")
 p1 = Polynomial2([0, 1, 1, 0, 0, 1])
 p2 = Polynomial2([1, 0, 1, 1])
 p3 = p1.add(p2)
-print "p3= p1+p2 = ", p3
+print("p3= p1+p2 = ", p3)
 
-print "\nTest 2"
-print "======"
-print "p4=x^7+x^4+x^3+x^2+x"
-print "modp=x^8+x^7+x^5+x^4+1"
+print("\nTest 2")
+print("======")
+print("p4=x^7+x^4+x^3+x^2+x")
+print("modp=x^8+x^7+x^5+x^4+1")
 p4 = Polynomial2([0, 1, 1, 1, 1, 0, 0, 1])
-# modp=Polynomial2([1,1,0,1,1,0,0,0,1])
+# modp = Polynomial2([1,1,0,1,1,0,0,0,1])
 modp = Polynomial2([1, 0, 0, 0, 1, 1, 0, 1, 1])
 p5 = p1.mul(p4, modp)
-print "p5=p1*p4 mod (modp)=", p5
+print("p5=p1*p4 mod (modp)=", p5)
 
-print "\nTest 3"
-print "======"
-print "p6=x^12+x^7+x^2"
-print "p7=x^8+x^4+x^3+x+1"
+print("\nTest 3")
+print("======")
+print("p6=x^12+x^7+x^2")
+print("p7=x^8+x^4+x^3+x+1")
 p6 = Polynomial2([0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1])
 p7 = Polynomial2([1, 1, 0, 1, 1, 0, 0, 0, 1])
 p8q, p8r = p6.div(p7)
-print "q for p6/p7=", p8q
-print "r for p6/p7=", p8r
+print("q for p6/p7=", p8q)
+print("r for p6/p7=", p8r)
 
 ####
-print "\nTest 4"
-print "======"
+print("\nTest 4")
+print("======")
 g1 = GF2N(100)
 g2 = GF2N(5)
-print "g1 = ", g1.getPolynomial2()
-print "g2 = ", g2.getPolynomial2()
+print("g1 = ", g1.getPolynomial2())
+print("g2 = ", g2.getPolynomial2())
 g3 = g1.add(g2)
-print "g1+g2 = ", g3
+print("g1+g2 = ", g3)
 
-print "\nTest 5"
-print "======"
+print("\nTest 5")
+print("======")
 ip = Polynomial2([1, 1, 0, 0, 1])
-print "irreducible polynomial", ip
+print("irreducible polynomial", ip)
 g4 = GF2N(0b1101, 4, ip)
 g5 = GF2N(0b110, 4, ip)
-print "g4 = ", g4.getPolynomial2()
-print "g5 = ", g5.getPolynomial2()
+print("g4 = ", g4.getPolynomial2())
+print("g5 = ", g5.getPolynomial2())
 g6 = g4.mul(g5)
-print "g4 x g5 = ", g6.p
+print("g4 x g5 = ", g6.p)
 
-print "\nTest 6"
-print "======"
+print("\nTest 6")
+print("======")
 g7 = GF2N(0b1000010000100, 13, None)
 g8 = GF2N(0b100011011, 13, None)
-print "g7 = ", g7.getPolynomial2()
-print "g8 = ", g8.getPolynomial2()
+print("g7 = ", g7.getPolynomial2())
+print("g8 = ", g8.getPolynomial2())
 q, r = g7.div(g8)
-print "g7/g8 ="
-print "q = ", q.getPolynomial2()
-print "r = ", r.getPolynomial2()
+print("g7/g8 =")
+print("q = ", q.getPolynomial2())
+print("r = ", r.getPolynomial2())
 
-print "\nTest 7"
-print "======"
+print("\nTest 7")
+print("======")
 ip = Polynomial2([1, 1, 0, 0, 1])
-print "irreducible polynomial", ip
+print("irreducible polynomial", ip)
 g9 = GF2N(0b101, 4, ip)
-print "g9 = ", g9.getPolynomial2()
-print "inverse of g9 =", g9.mulInv().getPolynomial2()
+print("g9 = ", g9.getPolynomial2())
+print("inverse of g9 =", g9.mulInv().getPolynomial2())
 
-print "\nTest 8"
-print "======"
+print("\nTest 8")
+print("======")
 ip = Polynomial2([1, 1, 0, 1, 1, 0, 0, 0, 1])
-print "irreducible polynomial", ip
+print("irreducible polynomial", ip)
 g10 = GF2N(0xC2, 8, ip)
-print "g10 = 0xc2"
+print("g10 = 0xc2")
 g11 = g10.mulInv()
-print "inverse of g10 = g11 =", hex(g11.getInt())
+print("inverse of g10 = g11 =", hex(g11.getInt()))
 g12 = g11.affineMap()
-print "affine map of g11 =", hex(g12.getInt())
+print("affine map of g11 =", hex(g12.getInt()))
